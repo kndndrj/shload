c1739da6f08581159d489e75c3c6ec2a8959d0a7
Chemaclass <chemaclass@outlook.es>
2024-09-01 13:24:31 +0200

    test: shload_setup with small, medium and big screen

---
+++ tests/shload_setup_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function set_up_before_script() {
+  _ROOT_DIR="$(dirname "${BASH_SOURCE[0]}")/.."
+
+  source "$_ROOT_DIR/shload.sh"
+}
+
+function test_small_screen() {
+  mock tput echo 20
+
+  assert_equals "Progress:[s [[0C] 0%"\
+    "$(shload_setup 10 '+')"
+}
+
+function test_medium_screen() {
+  mock tput echo 21
+
+  assert_equals "Progress:[s [[1C] 0%"\
+    "$(shload_setup 10 '+')"
+}
+
+
+function test_big_screen() {
+  mock tput echo 50
+
+  assert_equals "Progress:[s [[24C] 0%"\
+    "$(shload_setup 10 '+')"
+}
